e514d17f0893647b39368d84e65c277b420db09f
SwabhimanNanda <swabhiman2025@gmail.com>
2024-07-02 23:20:16 +0530

    added clean up

--- src/components/Wfinder.jsx
+++ src/components/Wfinder.jsx
@@ -1,22 +1,26 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const Wfinder = () => {
-    const [width , setWidth ] = useState(window.screen.width)
-    const [height , setHeight ] = useState(window.screen.height)
-    let check = ()=>{
-        setHeight(window.innerHeight)
-        setWidth(window.innerWidth)
-    }
-    useEffect(()=>{
-     window.addEventListener("resize" , check)   
-    })
+  const [width, setWidth] = useState(window.screen.width);
+  const [height, setHeight] = useState(window.screen.height);
+  let check = () => {
+    setHeight(window.innerHeight);
+    setWidth(window.innerWidth);
+  };
+  useEffect(() => {
+    console.log("started");
+    window.addEventListener("resize", check);
+    return () => {
+      window.removeEventListener("resize", check);
+    };
+  });
   return (
-    <div className='flex flex-col justify-center items-center h-screen gap-y-24'>
-      <p className='text-9xl text-center'>Know your screen Width</p>
-      <h1 className='text-6xl'>Width :{width}</h1>
-      <h1 className='text-6xl'>height :{height}</h1>
+    <div className="flex flex-col justify-center items-center h-screen gap-y-24">
+      <p className="text-9xl text-center">Know your screen Width</p>
+      <h1 className="text-6xl">Width :{width}</h1>
+      <h1 className="text-6xl">height :{height}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Wfinder
+export default Wfinder;
