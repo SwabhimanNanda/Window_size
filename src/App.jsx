4ccb8c64921b2ad9caf8cb6315d999404e235b8b
SwabhimanNanda <swabhiman2025@gmail.com>
2024-07-02 20:52:02 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import "./App.css";
+import Wfinder from "./components/Wfinder";
+function App() {
+  return <div style={{display:'flex' , flexDirection:'column' , justifyContent:'center' , alignItems:"center"}}>
+  <Wfinder/>
+  </div>;
+}
+
+export default App;
--- widthbtao/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import Wfinder from "./components/Wfinder";
-function App() {
-  return <>
-  <Wfinder/>
-  </>;
-}
-
-export default App;
